64f910b9231109fe0f5e7afb50b927c81f002b2b
aryaskuyy <139629221+aryaskuyy@users.noreply.github.com>
2023-07-24 12:03:50 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Simple To-Do List</title>
+</head>
+<body>
+    <h1>Simple To-Do List</h1>
+    <form action="add_task.php" method="post">
+        <input type="text" name="task" placeholder="Tambahkan tugas baru" required>
+        <button type="submit">Tambah</button>
+    </form>
+    <ul>
+        <?php
+        // Kode PHP untuk mengambil dan menampilkan daftar tugas dari database
+        ?>
+    </ul>
+</body>
+</html>
